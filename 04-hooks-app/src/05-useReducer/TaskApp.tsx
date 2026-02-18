68f418e72da15032427e633c33d844bed9762204
Daniel <danieltoqui96@gmail.com>
2026-02-17 22:15:51 -0300

    hooksApp - local storage and zod validator

--- 04-hooks-app/src/05-useReducer/TaskApp.tsx
+++ 04-hooks-app/src/05-useReducer/TaskApp.tsx
@@ -1,4 +1,4 @@
-import { useState, useReducer } from "react";
+import { useState, useReducer, useEffect } from "react";
 
 import { Plus, Trash2, Check } from "lucide-react";
 
@@ -8,45 +8,27 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { taskReducer, getTasksInitialState } from "./reducer/taskReducer";
 
-// interface Todo {
-//   id: number;
-//   text: string;
-//   completed: boolean;
-// }
-
 export const TasksApp = () => {
   // const [todos, setTodos] = useState<Todo[]>([]);
   const [inputValue, setInputValue] = useState("");
   const [state, dispatch] = useReducer(taskReducer, getTasksInitialState());
 
+  useEffect(() => {
+    localStorage.setItem("tasks-state", JSON.stringify(state));
+  }, [state]);
+
   const addTodo = () => {
     if (inputValue.length === 0) return;
     dispatch({ type: "ADD_TODO", payload: inputValue });
-    // const newTodo: Todo = {
-    //   id: Date.now(),
-    //   text: inputValue.trim(),
-    //   completed: false,
-    // };
-
-    // setTodos([...todos, newTodo]);
     setInputValue("");
   };
 
   const toggleTodo = (id: number) => {
     dispatch({ type: "TOGGLE_TODO", payload: id });
-    // const updatedTodos = todos.map((todo) => {
-    //   if (todo.id === id) {
-    //     return { ...todo, completed: !todo.completed };
-    //   }
-    //   return todo;
-    // });
-    // setTodos(updatedTodos);
   };
 
   const deleteTodo = (id: number) => {
     dispatch({ type: "DELETE_TODO", payload: id });
-    // const updatedTodos = todos.filter((todo) => todo.id !== id);
-    // setTodos(updatedTodos);
   };
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
--- 04-hooks-app/src/05-useReducer/reducer/taskReducer.ts
+++ 04-hooks-app/src/05-useReducer/reducer/taskReducer.ts
@@ -1,3 +1,5 @@
+import * as z from "zod";
+
 interface Todo {
   id: number;
   text: string;
@@ -16,13 +18,41 @@ export type TaskAction =
   | { type: "TOGGLE_TODO"; payload: number }
   | { type: "DELETE_TODO"; payload: number };
 
+const TodoSchema = z.object({
+  id: z.number(),
+  text: z.string(),
+  completed: z.boolean(),
+});
+
+const TaskStateScheme = z.object({
+  todos: z.array(TodoSchema),
+  length: z.number(),
+  completed: z.number(),
+  pending: z.number(),
+});
+
 export const getTasksInitialState = (): TaskState => {
-  return {
-    todos: [],
-    completed: 0,
-    pending: 0,
-    length: 0,
-  };
+  const localStorageState = localStorage.getItem("tasks-state");
+  if (!localStorageState) {
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+
+  const result = TaskStateScheme.safeParse(JSON.parse(localStorageState));
+  if (result.error) {
+    return {
+      todos: [],
+      completed: 0,
+      pending: 0,
+      length: 0,
+    };
+  }
+
+  return result.data;
 };
 
 export const taskReducer = (
@@ -40,7 +70,7 @@ export const taskReducer = (
       return {
         ...state,
         length: state.todos.length + 1,
-        pending: state.completed + 1,
+        pending: state.pending + 1,
         todos: [...state.todos, newTodo],
       };
     }
