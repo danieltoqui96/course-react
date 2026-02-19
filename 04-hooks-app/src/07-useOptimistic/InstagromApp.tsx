b19003fa3209203100c2192ed58b27d57c26c3a8
Daniel <danieltoqui96@gmail.com>
2026-02-18 22:32:19 -0300

    use suspense

--- 04-hooks-app/src/07-useOptimistic/InstagromApp.tsx
+++ 04-hooks-app/src/07-useOptimistic/InstagromApp.tsx
@@ -1,4 +1,5 @@
 import { useOptimistic, useState, useTransition } from "react";
+import { toast } from "sonner";
 
 interface Comment {
   id: number;
@@ -39,13 +40,21 @@ export const InstagromApp = () => {
     startTransition(async () => {
       await new Promise((resolve) => setTimeout(resolve, 3000));
 
-      setComments((prev) => [
-        ...prev,
-        {
-          id: new Date().getTime(),
-          text: messageText,
-        },
-      ]);
+      // setComments((prev) => [
+      //   ...prev,
+      //   {
+      //     id: new Date().getTime(),
+      //     text: messageText,
+      //   },
+      // ]);
+
+      setComments((prev) => prev);
+      toast("Error al agregar comentario", {
+        description: "Intente nuevamente",
+        duration: 10_000,
+        position: "top-right",
+        action: { label: "Cerrar", onClick: () => toast.dismiss() },
+      });
     });
   };
 
--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,5 +1,6 @@
-// import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "sonner";
 // import { HooksApp } from "./HooksApp";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
@@ -11,12 +12,15 @@ import { createRoot } from "react-dom/client";
 // import { ScrambleWords } from "./05-useReducer/ScrambleWordReduce";
 // import { MemoHook } from "./06-memos/MemoHook";
 // import { MemoCounter } from "./06-memos/MemoCounter";
-import { InstagromApp } from "./07-useOptimistic/InstagromApp";
+// import { InstagromApp } from "./07-useOptimistic/InstagromApp";
+import { ClientInformation } from "./08-use-suspense/ClientInformation";
 
 import "./index.css";
+import { getUserAction } from "./08-use-suspense/api/get-user.action";
 
 createRoot(document.getElementById("root")!).render(
-  <>
+  <StrictMode>
+    <Toaster />
     {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
@@ -28,6 +32,15 @@ createRoot(document.getElementById("root")!).render(
     {/* <ScrambleWords /> */}
     {/* <MemoHook /> */}
     {/* <MemoCounter /> */}
-    <InstagromApp />
-  </>,
+    {/* <InstagromApp /> */}
+    <Suspense
+      fallback={
+        <div className="bg-gradient flex flex-col">
+          <h1 className="text-2xl">Cargando</h1>
+        </div>
+      }
+    >
+      <ClientInformation getUser={getUserAction(1000)} />
+    </Suspense>
+  </StrictMode>,
 );
