365bbaeb76b3b3da60b6d813a8c088503a1a4ebb
Daniel <danieltoqui96@gmail.com>
2026-02-24 13:32:49 -0300

    (05-heroesapp): add main views and configure routing

---
+++ 05-heros-app/src/HeroesApp.tsx
@@ -0,0 +1,6 @@
+import { RouterProvider } from "react-router/dom";
+import { appRouter } from "./router/app.router";
+
+export const HeroesApp = () => {
+  return <RouterProvider router={appRouter} />;
+};
