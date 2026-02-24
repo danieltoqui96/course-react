0225cdabf59f04a160998df52d360cb1bde1b65e
Daniel <danieltoqui96@gmail.com>
2026-02-24 18:02:57 -0300

    (05-heroesapp): add lazy loading to SearchPage

--- 05-heros-app/src/router/app.router.tsx
+++ 05-heros-app/src/router/app.router.tsx
@@ -1,11 +1,14 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
 
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { HomePage } from "@/heroes/pages/home/HomePage";
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
+// import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { AdminLayout } from "@/admin/layouts/AdminLayout";
 import { AdminPage } from "@/admin/pages/AdminPage";
+
+const SearchPage = lazy(() => import("@/heroes/pages/search/SearchPage"));
 
 export const appRouter = createBrowserRouter([
   {
