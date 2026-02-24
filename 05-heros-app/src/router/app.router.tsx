53af0befa288d6242b2855ac37d35486d9b0edab
Daniel <danieltoqui96@gmail.com>
2026-02-24 17:03:23 -0300

    (05-heroesapp): add layout component

--- 05-heros-app/src/router/app.router.tsx
+++ 05-heros-app/src/router/app.router.tsx
@@ -1,24 +1,39 @@
-import { AdminPage } from "@/admin/pages/AdminPage";
-import { HeroPage } from "@/heroes/pages/hero/HeroPage";
-import { HomePage } from "@/heroes/pages/home/HomePage";
-import { SearchPage } from "@/heroes/pages/search/SearchPage";
 import { createBrowserRouter } from "react-router";
+
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { HomePage } from "@/heroes/pages/home/HomePage";
+import { HeroPage } from "@/heroes/pages/hero/HeroPage";
+import { SearchPage } from "@/heroes/pages/search/SearchPage";
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
+import { AdminPage } from "@/admin/pages/AdminPage";
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/heroes/1",
-    element: <HeroPage />,
-  },
-  {
-    path: "/search",
-    element: <SearchPage />,
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "heroes/1",
+        element: <HeroPage />,
+      },
+      {
+        path: "search",
+        element: <SearchPage />,
+      },
+    ],
   },
   {
     path: "/admin",
-    element: <AdminPage />,
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />,
+      },
+    ],
   },
 ]);
