32c7bdc3f44aa75744b1315244db8b83deabbed8
Daniel <danieltoqui96@gmail.com>
2026-02-25 12:59:48 -0300

    (05-heroesapp): add HeroStats and HeroStatCard components

--- 05-heros-app/src/heroes/pages/search/SearchPage.tsx
+++ 05-heros-app/src/heroes/pages/search/SearchPage.tsx
@@ -1,11 +1,16 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
 
 export const SearchPage = () => {
   return (
-    <CustomJumbotron
-      title="Búsqueda de SuperHéroes"
-      description="Descubre, explora y administra super héroes y villanos "
-    />
+    <>
+      <CustomJumbotron
+        title="Búsqueda de SuperHéroes"
+        description="Descubre, explora y administra super héroes y villanos "
+      />
+      {/* Stats Dashboard */}
+      <HeroStats />
+    </>
   );
 };
 
