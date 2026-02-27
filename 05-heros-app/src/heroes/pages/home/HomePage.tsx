953eafc5f2d4e0ab5f05ba0fece1cfdb2a2cc5b5
Daniel <danieltoqui96@gmail.com>
2026-02-27 14:13:22 -0300

    (05-heroesapp): implement pagination

--- 05-heros-app/src/heroes/pages/home/HomePage.tsx
+++ 05-heros-app/src/heroes/pages/home/HomePage.tsx
@@ -14,6 +14,8 @@ export const HomePage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const activeTab = searchParams.get("tab") ?? "all";
+  const page = searchParams.get("page") ?? "1";
+  const limit = searchParams.get("limit") ?? "6";
 
   const selectedTab = useMemo(() => {
     const validTabs = ["all", "favorites", "heroes", "villains"];
@@ -21,8 +23,8 @@ export const HomePage = () => {
   }, [activeTab]);
 
   const { data: heroesResponse } = useQuery({
-    queryKey: ["heroes"],
-    queryFn: () => getHeroesByPageAction(),
+    queryKey: ["heroes", { page, limit }],
+    queryFn: () => getHeroesByPageAction(+page, +limit),
     staleTime: 1000 * 60 * 5,
   });
 
@@ -108,7 +110,7 @@ export const HomePage = () => {
         </Tabs>
 
         {/* Pagination */}
-        <CustomPagination totalPages={1} />
+        <CustomPagination totalPages={heroesResponse?.pages ?? 1} />
       </>
     </>
   );
