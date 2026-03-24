730560c303fdeeee141c518b9ec5cf8be2321f2a
Daniel <danieltoqui96@gmail.com>
2026-03-24 11:37:54 -0300

    (05-heroesapp): add Vitest tests for AppRouter Memory Router

--- 05-heros-app/src/router/app.router.test.tsx
+++ 05-heros-app/src/router/app.router.test.tsx
@@ -1,7 +1,12 @@
 import { describe, expect, test, vi } from 'vitest';
 import { appRouter } from './app.router';
 import { render, screen } from '@testing-library/react';
-import { Outlet, RouterProvider } from 'react-router';
+import {
+  createMemoryRouter,
+  Outlet,
+  RouterProvider,
+  useParams,
+} from 'react-router';
 
 vi.mock('@/heroes/layouts/HeroesLayout', () => ({
   HeroesLayout: () => (
@@ -15,14 +20,43 @@ vi.mock('@/heroes/pages/home/HomePage', () => ({
   HomePage: () => <div data-testid="home-page"></div>,
 }));
 
+vi.mock('@/heroes/pages/hero/HeroPage', () => ({
+  HeroPage: () => {
+    const { idSlug = '' } = useParams();
+    return <div data-testid="hero-page">HeroPage - {idSlug}</div>;
+  },
+}));
+
+vi.mock('@/heroes/pages/search/SearchPage', () => ({
+  default: () => <div data-testid="search-page"></div>,
+}));
+
 describe('appRouter', () => {
   test('should be configured as expected', () => {
     expect(appRouter.routes).toMatchSnapshot();
   });
 
   test('should render home page at root path', () => {
-    render(<RouterProvider router={appRouter} />);
-    screen.debug();
+    const router = createMemoryRouter(appRouter.routes, {
+      initialEntries: ['/'],
+    });
+    render(<RouterProvider router={router} />);
     expect(screen.getByTestId('home-page')).toBeDefined();
+  });
+
+  test('should render hero page /heroes/:idSlug path', () => {
+    const router = createMemoryRouter(appRouter.routes, {
+      initialEntries: ['/heroes/superman'],
+    });
+    render(<RouterProvider router={router} />);
+    expect(screen.getByTestId('hero-page').innerHTML).toContain('superman');
+  });
+
+  test('should render search page at /search path', async () => {
+    const router = createMemoryRouter(appRouter.routes, {
+      initialEntries: ['/search'],
+    });
+    render(<RouterProvider router={router} />);
+    expect(await screen.findByTestId('search-page')).toBeDefined();
   });
 });
